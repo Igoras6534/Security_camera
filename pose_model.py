963544b07f493ec34a312817e8a7aef842515cd2
Igoras6534 <ignatiukigor48@gmail.com>
2023-04-10 02:14:31 +0200

    Initial
    Basic Logic

---
+++ pose_model.py
@@ -0,0 +1,121 @@
+import cv2
+import mediapipe as mp
+import time
+import math
+
+class poseDetector():
+    def __init__(self, mode=False,
+               model_complexity=1,
+               smooth_landmarks=True,
+               enable_segmentation=False,
+               smooth_segmentation=True,
+               min_detection_confidence=0.5,
+               min_tracking_confidence=0.5):
+          
+        self.mode=mode
+        self.model_complexity= model_complexity
+        self.smooth_landmarks=smooth_landmarks
+        self.enable_segmentation=enable_segmentation
+        self.smooth_segmentation=smooth_segmentation
+        self.min_detection_confidence=min_detection_confidence
+        self.min_tracking_confidence= min_tracking_confidence
+        self.mpDraw=mp.solutions.drawing_utils
+        self.mpPose=mp.solutions.pose
+        self.pose=self.mpPose.Pose(self.mode,self.model_complexity,
+                                   self.smooth_landmarks,self.enable_segmentation,
+                                   self.smooth_segmentation,
+                                   self.min_detection_confidence,
+                                   self.min_tracking_confidence,)
+        self.drawing_spec=self.mpDraw.DrawingSpec(color=(0,255,0))
+
+
+
+
+    def findPose(self,frame,draw=True):
+        frame_rgb=cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
+        self.results=self.pose.process(frame_rgb)
+        if self.results.pose_landmarks:
+            pose_found=True
+            if draw:
+                self.mpDraw.draw_landmarks(frame,self.results.pose_landmarks, 
+                                           self.mpPose.POSE_CONNECTIONS,connection_drawing_spec=self.drawing_spec)
+        else:
+            pose_found=False
+        return frame, pose_found
+        
+    def getPosition(self,frame,draw=True):
+        self.lmlist=[]
+        if self.results.pose_landmarks:
+            for id, lm in enumerate(self.results.pose_landmarks.landmark):
+                h,w,c=frame.shape
+                cx,cy=int(lm.x*w), int(lm.y*h)
+                self.lmlist.append([id,cx,cy])
+                if draw:
+                    cv2.circle(frame,(cx,cy),7,(255,0,255),4)
+        return self.lmlist
+
+    def findAngle(self,frame, p1, p2, p3, draw=True):
+
+
+
+        x1,y1=self.lmlist[p1][1:]
+        x2,y2=self.lmlist[p2][1:]
+        x3,y3=self.lmlist[p3][1:]
+
+        #------------------------------------
+        angle=int(math.degrees(math.atan2(y3-y2,x3-x2)-math.atan2(y1-y2,x1-x2)))
+        #print(angle)
+        if angle<0:
+            angle+=360
+
+
+        #------------------------------------
+        if draw:
+            cv2.line(frame,(x1,y1),(x2,y2),(255,255,255),3)
+            cv2.line(frame,(x2,y2),(x3,y3),(255,255,255),3)
+
+            cv2.circle(frame,(x1,y1),10,(0,0,255),cv2.FILLED)
+            cv2.circle(frame,(x1,y1),15,(0,0,255),2)
+
+            cv2.circle(frame,(x2,y2),10,(0,0,255),cv2.FILLED)
+            cv2.circle(frame,(x2,y2),15,(0,0,255),2)
+
+            cv2.circle(frame,(x3,y3),10,(0,0,255),cv2.FILLED)
+            cv2.circle(frame,(x3,y3),15,(0,0,255),2)
+            cv2.putText(frame,str(angle),(x2-50,y2+50),cv2.FONT_HERSHEY_PLAIN,
+                        2,(0,0,255),2)
+        return angle
+
+
+
+
+    
+
+def main():
+    cam=cv2.VideoCapture(1)
+    c_time=0
+    p_time=0
+    detector=poseDetector()
+    while True:
+        _,frame=cam.read()
+        frame=detector.findPose(frame)
+        lmlist=detector.getPosition(frame)
+        print(lmlist)
+
+
+
+        c_time=time.time()
+        fps=1/(c_time-p_time)
+        p_time=c_time
+        cv2.putText(frame,str(int(fps)),(70,50),cv2.FONT_HERSHEY_PLAIN,
+                    3,(255,0,255),3)
+        cv2.imshow("Camera",frame)
+        
+        if cv2.waitKey(1)==ord("q"):
+            break
+
+
+
+
+if __name__=="__main__":
+    main()
---
+++ security_camera.py
@@ -0,0 +1,70 @@
+import cv2
+import pose_model as pm
+import datetime
+import time
+
+
+
+
+cam=cv2.VideoCapture(1)
+frame_size=(int(cam.get(3)),int(cam.get(4)))
+
+c_time=0
+p_time=0
+detector=pm.poseDetector()
+
+
+
+
+
+
+detection=False
+detection_stopped_time=None
+timer_started=False
+seconds=7
+fourcc=cv2.VideoWriter_fourcc(*"mp4v")
+
+
+while True:
+    _,frame=cam.read()
+    frame=cv2.flip(frame,1)
+    frame,pose_found=detector.findPose(frame,False)
+    if pose_found==True:
+        if detection == True:
+            timer_started=False
+        else:
+            detection=True
+            current_time=datetime.datetime.now().strftime("%d-%m-%Y-%H-%M-%S")
+            out=cv2.VideoWriter(f"videos/{current_time}.mp4",fourcc,20,frame_size)
+            print("Started recording")
+    elif pose_found== False:
+        if timer_started:
+            if time.time()-detection_stopped_time>=seconds:
+                detection=False
+                timer_started=False
+                out.release()
+                print("Stopped Recording")
+        else:
+            timer_started=True
+            detection_stopped_time=time.time()
+    if detection == True:
+        out.write(frame)
+
+
+
+
+    c_time=time.time()
+    fps=int(1/(c_time-p_time))
+    p_time=c_time
+
+    cv2.putText(frame,f"FPS:{fps}",(30,70),cv2.FONT_HERSHEY_PLAIN
+                ,2,(255,0,0),2)
+    cv2.imshow("Cam",frame)
+        
+    if cv2.waitKey(1)==ord("q"):
+        break
+
+out.release()
+cam.release()
+cv2.destroyAllWindows()
+
